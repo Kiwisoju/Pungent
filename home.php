6b01160cb4849b9866b59576a7178cc34031a855
kiwisoju <glennjamesforrest@gmail.com>
2015-07-22 12:04:06 +0000

    All posts writing to the database

--- home.php
+++ home.php
@@ -8,7 +8,12 @@ $authenticator->redirectUnauthenticatedUser();
 //$db = new DatabaseHelper();
 include('header.php');
 
-
+if($_POST){
+        // Setting the $_POST data to $data so as to set the values
+        // in the form set to what was previously entered.
+        $data = $_POST; 
+        $databaseQueries->addPun($data);
+    }
 /** pun of the day
  * Will use the date function to check what the present
  * date is, and pull the row where the date matches.
@@ -18,10 +23,13 @@ include('header.php');
 <!-- Page Content -->
     <div class="container page-content text-center">
       <?php if($_GET['not-admin']=='yes'):?>
-        <div class="popup header-dropshadow"><p>Sorry, you don't have permission to enter that page.</p> </div>
-      <?php endif; ?>
-      <?php if($_GET['loggedin']=='yes'):?>
+        <div class="popup"><p>Sorry, you don't have permission to enter that page.</p> </div>
+      <?php elseif($_GET['loggedin']=='yes'):?>
         <div class="popup"><p>Succesfully logged in.</p> </div>
+      <?php elseif($_GET['pun']=='yes'):?>
+        <div class="popup"><p>Pun post successful!</p> </div>
+      <?php elseif($_GET['pun']=='no'): ?>
+        <div class="popup"><p>Pun post un-successful</p> </div>
       <?php endif; ?>
       <div class="pun-of-the-day">
         <h2>Pun of the day</h2>
@@ -29,7 +37,7 @@ include('header.php');
       </div>
       <hr class="hr-fade">
       <div class="topic challenge">
-        <h2><a href="?topic-challenge3">Topic Challenge #3 <br>Trees</a></h2>
+        <h2><a href="?topic-challenge3">Topic Challenge #<?=$databaseQueries->getChallenge("topic_challenge")['id'] ?><br><?= $databaseQueries->getChallenge("topic_challenge")['topic']?></a></h2>
         <div class="pun-post">
             <p class="pun-date pull-left">26/06/2015</p>
             <div class="pun-inner row">
@@ -66,17 +74,17 @@ include('header.php');
             </div>
             <p class="username pull-right"><a href="?username-profile">Username</a></p>
         </div>
-        <form class="pun-input">
+        <form class="pun-input" method="post">
           <div class="form-group">
-              <input type="text" class="form-control text-center" placeholder="Post your pun here"></input>
+              <input type="text" class="form-control text-center" name="pun-topic" value="<?= $data['pun-topic']?>" placeholder="Post your pun here" required></input>
           </div>
-           <button type="submit" class="btn btn-default">Submit</button>
+           <input type="submit" class="btn btn-default">
         </form>
       </div>
       <hr class="hr-fade">
       <div class="image challenge">
-        <h2><a href="?image-challenge3">Image Challenge #3</a></h2>
-        <img class="grayscale img-responsive" src="images/image-challenge-2.jpg"/>
+        <h2><a href="?image-challenge3">Image Challenge #<?= $databaseQueries->getChallenge("image_challenge")['id']?></a></h2>
+        <img class="grayscale img-responsive" src="<?=$databaseQueries->getChallenge("image_challenge")['image'];?>"/>
       <div class="pun-post">
             <p class="pun-date pull-left">26/06/2015</p>
             <div class="pun-inner row">
@@ -113,12 +121,12 @@ include('header.php');
             </div>
             <p class="username pull-right"><a href="?username-profile">Username</a></p>
         </div>
-        <form class="pun-input">
+        <form class="pun-input" method="post">
           <div class="form-group">
-              <input type="text" class="form-control text-center" placeholder="Post your pun here"></input>
+              <input type="text" class="form-control text-center" name="pun-image" value="<?= $data['pun-image']?>" placeholder="Post your pun here" required></input>
           </div>
-           <button type="submit" class="btn btn-default">Submit</button>
-        </form>
+           <input type="submit" class="btn btn-default">
+        </form>  
       
       </div>
       <?php
